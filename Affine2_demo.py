9d47b5373304c5b3f82f095b03ac33d8b4315f5d
Bhop <107392788+B-h0p@users.noreply.github.com>
2022-06-14 08:26:17 +1000

    Updated Affine2
    Because how I did it before was dumb lol

--- Affine2_demo.py
+++ Affine2_demo.py
@@ -3,36 +3,32 @@ def encrypt(url, eq):
     url_chars = list(url)
     increment = 1
     for x in range(len(url_chars)):
-        if url_chars[x].islower():
+        make_shift = False
+        if url_chars[x].islower(): ascii_val, make_shift = 97, True
+        elif url_chars[x].isupper(): ascii_val, make_shift = 65, True
+        if make_shift:
             new_sum = 0
             for y in range(len(eq)): new_sum = new_sum + (eq[y] * (increment ** y)) #creates a polynomial that is binded to each alpha
-            url_chars[x] = chr((ord(url_chars[x]) + (new_sum%26) - 97) % 26 + 97)
+            url_chars[x] = chr((ord(url_chars[x]) + (new_sum%26) - ascii_val) % 26 + ascii_val)
             increment += 1 #so our cypher is never constant
-        elif url_chars[x].isupper():
-            new_sum = 0
-            for y in range(len(eq)): new_sum = new_sum + (eq[y] * (increment ** y))
-            url_chars[x] =  chr((ord(url_chars[x]) + (new_sum%26) - 65) % 26 + 65)
-            increment += 1
     return "".join(url_chars)
 
 def decrypt(url, eq):
     url_chars = list(url)
     increment = 1
     for x in range(len(url_chars)):
-        if url_chars[x].islower():
+        make_shift = False
+        if url_chars[x].islower(): ascii_val, make_shift = 97, True
+        elif url_chars[x].isupper(): ascii_val, make_shift = 65, True
+        if make_shift:
             new_sum = 0
-            for y in range(len(eq)): new_sum = new_sum + (eq[y] * (increment ** y))
-            url_chars[x] = chr((ord(url_chars[x]) - (new_sum%26) - 97) % 26 + 97) #(new_sum%26) hastens the calculation without changing the functions pattern
-            increment += 1
-        elif url_chars[x].isupper():
-            new_sum = 0
-            for y in range(len(eq)): new_sum = new_sum + (eq[y] * (increment ** y))    
-            url_chars[x] =  chr((ord(url_chars[x]) - (new_sum%26) - 65) % 26 + 65)
-            increment += 1 
+            for y in range(len(eq)): new_sum = new_sum + (eq[y] * (increment ** y)) #creates a polynomial that is binded to each alpha
+            url_chars[x] = chr((ord(url_chars[x]) - (new_sum%26) - ascii_val) % 26 + ascii_val)
+            increment += 1 #so our cypher is never constant
     return "".join(url_chars)
 
 print(encrypt("https://www.youtube.com/watch?v=g97La0u55_g&ab_channel=KILTLE", [32,19,5]))
-print(decrypt("lfxvk://kqg.iiilafq.gue/kudmb?j=y97Re0g55_k&gt_qbkxhsd=QMXXRW",[32,19,5]))
+print(decrypt(encrypt("https://www.youtube.com/watch?v=g97La0u55_g&ab_channel=KILTLE", [32,19,5]),[32,19,5]))
 print(decrypt("nhxra://sos.oqsurk.qso", [58,22,4])) #demo which was produced in sample by a randomised dt.json
 
 #I could of made them a single function but really I wasnt bothered lol
